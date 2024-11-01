6074250ee814b36618a88c68b75f13f47ae4c2d3
Perry-Cham <tylerchamileke@outlook.com>
2024-11-01 23:59:18 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const CTX = document.getElementById('expense-graph');
+let myData = [];
+
+fetch("http://localhost:8000/data.json").then(response =>
+  response.json()).then((response) => {
+    myData = response;
+    main();
+    console.log(myData)
+  })
+
+
+function main() {
+  const date = new Date();
+const today = date.toLocaleDateString('en-US', { weekday: 'short' });
+
+
+
+  
+  
+  
+  let Days = myData.map((child) => child.day)
+  let amounts = myData.map((child) => child.amount)
+  console.log(amounts);
+  
+  new Chart(CTX, {
+    type: 'bar',
+    data: {
+      labels:Days,
+      datasets: [{
+        label: 'Amount in USD',
+        data: amounts,
+        borderWidth: 1,
+        backgroundColor:function(context) {
+    return context.chart.data.labels[context.dataIndex] === today ? 'hsl(186, 34%, 60%)' : 'hsl(10, 79%, 65%)';
+}
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  
+  
+}
+  
+  
